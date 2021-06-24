248330b4912f01398332ee2477d4f8777b0dc85f
ealtidor <emanuella.altidor@gmail.com>
2021-06-23 22:14:34 -0400

    added property service file

---
+++ client/src/services/property.js
@@ -0,0 +1,30 @@
+import api from "./api-config";
+
+export const getAllProperties = async () => {
+  const resp = await api.get("/properties");
+  return resp.data;
+};
+
+export const getOneProperty = async (id) => {
+  const resp = await api.get(`/properties/${id}`);
+  return resp.data;
+};
+
+export const postProperty = async (propertyData) => {
+  const resp = await api.post("/properties", { property: propertyData });
+  return resp.data;
+};
+
+export const putProperty = async (id, propertyData) => {
+  const resp = await api.put(`/properties/${id}`, { property: propertyData });
+  return resp.data;
+};
+
+export const deleteProperty = async (id) => {
+  const resp = await api.delete(`/properties/${id}`);
+  return resp;
+};
+
+export const addTenant = async (propertyId, tenantId) => {
+  const resp = await api.put(`/tenants/${tenantId}/properties/${propertyId}`);
+};
