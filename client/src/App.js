495d12b584caeab7199771cfaedafb6e444522cb
ealtidor <emanuella.altidor@gmail.com>
2021-06-24 00:36:45 -0400

    Complete login,logout,and register functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,22 @@ import Layout from "./layout/Layout";
 import { Switch, Route, useHistory } from "react-router-dom";
 import Login from "./screens/Login/Login";
 import Register from './screens/Register/Register'
-import { useState } from 'react'
-import { loginUser, registerUser } from './services/auth'
+import { useState, useEffect } from 'react'
+import { loginUser, registerUser, verifyUser, removeToken } from './services/auth'
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const history = useHistory()
 
+  useEffect(() => {
+		const handleVerify = async () => {
+			const userData = await verifyUser();
+			setCurrentUser(userData);
+		};
+		handleVerify();
+	}, []);
+  
+  
   const handleLogin = async (formData) => {
     const userData = await loginUser(formData)
     setCurrentUser(userData)
@@ -22,10 +31,15 @@ function App() {
 		history.push('/');
 	};
 
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+  }
 
   return (
     <div className="App">
-      <Layout>
+      <Layout currentUser={currentUser} handleLogout={handleLogout}>
         <Switch>
           <Route path="/properties">
             <h3>PROPERTIES</h3>
--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,19 +1,29 @@
-import './Layout.css'
-import { Link } from 'react-router-dom'
+import "./Layout.css";
+import { Link } from "react-router-dom";
 
 export default function Layout(props) {
+  const { currentUser, handleLogout } = props;
   return (
     <div>
+      <h1>TOGGLE</h1>
       <header>
-        <h1>TOGGLE</h1>
-        <Link to='/properties'>PROPERTIES</Link>
-        <Link to='/login'>LOGIN</Link>
-        <Link to='/register'>REGISTER</Link>
+        {currentUser ? (
+          <>
+            <Link to="/properties">PROPERTIES</Link>
+            <p>{currentUser.username}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">LOGIN</Link>
+            <Link to="/register">REGISTER</Link>
+          </>
+        )}
       </header>
       {props.children}
       <footer>
         <p>© Emanuella Altidor, 2021</p>
       </footer>
     </div>
-  )
+  );
 }
