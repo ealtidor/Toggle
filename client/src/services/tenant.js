ff0278250322a8ce9d425860a80c47617307aa14
ealtidor <emanuella.altidor@gmail.com>
2021-06-23 22:05:59 -0400

    added tenant service file

---
+++ client/src/services/tenant.js
@@ -0,0 +1,27 @@
+import api from './api-config'
+
+export const getAllTenants = async () => {
+  const resp = await api.get('/tenants');
+  return resp.data
+};
+
+export const getOneTenant = async (id) => {
+  const resp = await api.get(`/tenants/${id}`)
+  return resp.data 
+}
+
+export const postTenant = async (tenantData) => {
+  const resp = await api.post('/tenants', { tenant: tenantData });
+  return resp.data
+}
+
+export const putTenant = async (id, tenantData) => {
+  const resp = await api.put(`/tenants/${id}`, { tenant: tenantData })
+  return resp.data
+}
+
+export const deleteTenant = async (id) => {
+  const resp = await api.delete(`/tenants/${id}`)
+  return resp;
+}
+
